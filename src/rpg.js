f549738910b32ed3aec8c54d3073764f007d96f7
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2023-08-30 19:48:17 -0300

    feat rpg inport lib

---
+++ src/rpg.js
@@ -0,0 +1,6 @@
+import { NPC } from "./characters/NPC.js";
+import { Player } from "./characters/Player.js";
+import { Scene } from "./scenes/Scene.js";
+import { Game } from "./settings/Game.js";
+
+export { Game, NPC, Player, Scene };
